24a63311fe2279f88f4a339a670128d2baf95752
tom murdoch <tmurdoch_99@yahoo.com>
2014-02-25 14:24:59 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betbrg::Application.config.secret_token = 'b5afc9d29e749e4662171c08341efa8d0bc9c7d34fd0471397fb07f9fd80fcefdbc1399d327fa135e6f54548418bb28761f03bddcd72081c1f0e2f73f3bb6970'
