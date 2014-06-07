5569b9010336d644f7ba5f20b4800cb30832a921
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-07 16:05:51 +0100

    cleared out less and css from fontawesome to pass precompile

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ziphire::Application.config.session_store :cookie_store, key: '_betbrg_session'
+Zipwire::Application.config.session_store :cookie_store, key: '_betbrg_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
