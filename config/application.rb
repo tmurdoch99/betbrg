85466f05158453958a1966110ec5cace242a138c
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-07 15:34:17 +0100

    changed assets compile true in env/pro1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-module ziphire
+module Zipwire
   class Application < Rails::Application
 
     # don't generate RSpec tests for views and helpers
