284160806542f7090130d0ffd9ec603b63c8bfd7
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-07 02:04:08 +0100

    hopefully solved roots to for diff users

---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,7 @@
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'L8IilTCPM8jImTnTMEk306429'
+  config.consumer_secret = 'AoOyzzqOkxUWHz1FbPzNZAgkRoUIwT6gIXFPCDwZivacRWORQD'
+  config.access_token  = '2508866504-DqkV6RpEUdg4wJ707pG9qOScHY4sCuHW1TG6lXF'
+  config.access_token_secret = 'oY8QPOzc44fBwy67P7Del1hvecPR65kx22nWev3e643Bt'
+end
