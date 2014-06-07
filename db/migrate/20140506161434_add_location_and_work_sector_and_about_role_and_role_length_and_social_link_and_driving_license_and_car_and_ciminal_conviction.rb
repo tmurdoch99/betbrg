864a28c8fbb366cbb88458c544f5abef98c3d610
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-07 17:28:03 +0100

    cleared out less and css from fontawesome to pass precompile

--- db/migrate/20140506161434_add_location_and_work_sector_and_about_role_and_role_length_and_social_link_and_driving_license_and_car_and_ciminal_conviction.rb
+++
@@ -1,7 +0,0 @@
-class AddLocationAndWorkSectorAndAboutRoleAndRoleLengthAndSocialLinkAndDrivingLicenseAndCarAndCiminalConviction < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20140605160822_add_logo_to_user_details.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoToUserDetails < ActiveRecord::Migration
-  def change
-    add_column :user_details, :logo, :string
-  end
-end
