15f955a1b03cb0209ded57c335296f3fd43ec2ae
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-07 15:27:35 +0100

    changed assets compile true in env/pro1

--- config/application.rb
+++ config/application.rb
@@ -75,7 +75,7 @@ module Betbrg
 
     # Enable the asset pipeline
 	config.assets.initialize_on_precompile=false 
-    config.assets.enabled = flase
+    config.assets.compress = false
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 	config.assets.paths << "#{Rails.root}/app/assets/plugins"
     # Version of your assets, change this if you want to expire all your assets
