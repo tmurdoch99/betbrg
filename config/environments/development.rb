c4473a680ff2d67ea5460069859b822bafb88483
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-10 15:36:06 +0100

    changed zipwire on .rbs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-zipwire::Application.configure do
+Zipwire::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-zipwire::Application.configure do
+Zipwire::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
