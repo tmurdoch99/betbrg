1b1cda91c37077006caaa3554ee11b55d7fdba02
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-11 20:21:20 +0100

    updated user model, getting rid of messenger etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,7 @@ class User < ActiveRecord::Base
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :logo, :password, :password_confirmation, :remember_me, :business
   validates_presence_of :name
-  include Amistad::FriendModel
-  acts_as_messageable
-  has_many :predictions
   has_many :user_details, dependent: :destroy
-  has_many :fighters, through: :predictions
-  accepts_nested_attributes_for :predictions
-  mount_uploader :logo, AvatarUploader
+  
  
 end
