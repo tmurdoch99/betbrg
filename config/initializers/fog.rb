a5a36fe68e5feb22ec5017b91b109e2c3912c896
tom murdoch <tmurdoch_99@yahoo.com>
2014-06-28 16:25:04 +0100

    changed back to pg

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAINA6GWBBBG4NFEAQ', 
     :aws_secret_access_key  => '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 ', 
   } 
-  config.fog_directory  = 'ziphire' 
+  config.fog_directory  = 'ziphire1' 
   config.fog_public     = false 
 end 
