73c2a38baf6eb37dd3f7fc7fa69cb7f88f1a6f9d
tom murdoch <tmurdoch_99@yahoo.com>
2014-07-01 16:08:47 +0100

    spelling mistakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['Tom Murdoch'].dup, :email => ENV['tmurdoch_99@yahoo.com'].dup, :password => ENV['hollins99'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :name => ENV['Tom Murdoch'].dup, :email => ENV['tmurdoch_99@yahoo.com'].dup, :password => ENV['hollins99'].dup, :password_confirmation => ENV['hollins99'].dup
 puts 'user: ' << user.name
 user.add_role :admin
 
